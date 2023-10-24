06a7eded46ae26b1162f89116d28f5d1a660506e
Elham Mirzavand <elham.mirzavand@gmail.com>
2023-10-24 10:53:37 +0200

    Create components + Complete Playlist component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import Login from './components/Login'
+import {useStateProvider} from './utils/statePovider'
+import { reducerCases } from "./utils/Constants";
+import Spotify from './components/Spotify';
+
 export default function App() {
-  return (
-    <div>App</div>
-  )
+  const [{ token }, dispatch] = useStateProvider();
+  useEffect(() => {
+    const hash = window.location.hash;
+    if (hash) {
+      const token = hash.substring(1).split("&")[0].split("=")[1];
+      dispatch({ type: reducerCases.SET_TOKEN, token })
+    }
+
+  }, [token, dispatch])
+  return <div> {token ? <Spotify /> : <Login />}</div>;
+  
 }
--- src/utils/reducer.js
+++ src/utils/reducer.js
@@ -1,8 +1,29 @@
+import { reducerCases } from "./Constants";
+
 export const initialState = {
     token: null,
+    playlists: [],
+    userInfo : null,
 };
 const reducer = (state, action) => {
     switch (action.type) {
+        case reducerCases.SET_TOKEN: {
+            return {
+                ...state, token: action.token
+            }
+        }
+        case reducerCases.SET_PLAYLIST: {
+            return {
+                ...state,
+                playlists: action.playlists
+            }
+        }
+        case reducerCases.SET_USER: {
+            return {
+                ...state,
+                userInfo: action.userInfo,
+            }
+        }
         default: return state;
     }
 };
